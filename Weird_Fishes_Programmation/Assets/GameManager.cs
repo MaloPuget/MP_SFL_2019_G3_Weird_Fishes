019c5977221420203fa2192b7d0b1903aa6ee78a
Valentin <gibouleauv@gmail.com>
2020-06-01 22:25:11 +0200

    Gestion de la barre de vie en général, ajout d'un background (exemple)
    Gestion des dégats (perte et regain de vie)

---
+++ Weird_Fishes_Programmation/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    void EndGame()
+    {
+       Debug.Log("GAME OVER");
+    }
+
+}
---
+++ Weird_Fishes_Programmation/Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Texture2D imgOptions;
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TakeDamage(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GetBonus(2);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if(currentHealth == 0)
+        {
+           Debug.Log("GAME OVER");
+        }
+    }
+
+    void GetBonus(int bonus)
+    {
+        currentHealth += bonus;
+        healthBar.SetHealth(currentHealth);
+    }
+}
